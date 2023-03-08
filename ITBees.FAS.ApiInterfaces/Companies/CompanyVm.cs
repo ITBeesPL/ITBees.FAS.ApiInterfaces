727ff3a8dfe866719a4119488b6d627200088a81
Jakub Zohar Rutkowski <kuba@skb.pl>
2023-03-08 22:33:39 +0100

    Added view models

---
+++ ITBees.FAS.ApiInterfaces/Companies/CompanyVm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ITBees.RestClient.Interfaces.RestModelMarkup;
+
+namespace ITBees.FAS.ApiInterfaces.Companies
+{
+    public class CompanyVm : Vm
+    {
+        public Guid Guid { get; set; }
+        public string CompanyName { get; set; }
+        public DateTime Created { get; set; }
+        public string CreatedBy { get; set; }
+        public Guid CreatedByGuid { get; set; }
+        public string Owner { get; set; }
+        public Guid OwnerGuid { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ ITBees.FAS.ApiInterfaces/MyAccounts/MyAccountVm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using ITBees.FAS.ApiInterfaces.Companies;
+using ITBees.RestClient.Interfaces.RestModelMarkup;
+
+namespace ITBees.FAS.ApiInterfaces.MyAccounts
+{
+    public class MyAccountVm : Vm
+    {
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Guid Guid { get; set; }
+        public List<CompanyVm> Companies { get; set; }
+        public CompanyVm LastUsedCompany { get; set; }
+        public Guid LastUsedCompanyGuid { get; set; }
+    }
+}
