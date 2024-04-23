e2fb90c535e8a2717eaf5f89ea096b4c4a0da6fe
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-04-23 16:18:02 +0200

    MyAccount UpdateModel support

---
+++ ITBees.FAS.ApiInterfaces/MyAccounts/MyAccountIm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ITBees.RestClient.Interfaces.RestModelMarkup;
+
+namespace ITBees.FAS.ApiInterfaces.MyAccounts
+{
+    public class MyAccountIm : Im
+    {
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Guid LastUsedCompanyGuid { get; set; }
+        public string Language { get; set; }
+    }
+}
