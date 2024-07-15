25f431a4439870a10fd28732109b63e287a06b2e
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-07-15 11:08:06 +0200

    Expand company view model

--- ITBees.FAS.ApiInterfaces/Companies/CompanyVm.cs
+++ ITBees.FAS.ApiInterfaces/Companies/CompanyVm.cs
@@ -9,9 +9,14 @@ namespace ITBees.FAS.ApiInterfaces.Companies
         public string CompanyName { get; set; }
         public DateTime Created { get; set; }
         public string CreatedBy { get; set; }
-        public Guid CreatedByGuid { get; set; }
+        public Guid? CreatedByGuid { get; set; }
         public string Owner { get; set; }
-        public Guid OwnerGuid { get; set; }
+        public Guid? OwnerGuid { get; set; }
         public bool IsActive { get; set; }
+        public string CompanyShortName { get; set; }
+        public string Street { get; set; }
+        public string PostCode { get; set; }
+        public string City { get; set; }
+        public string Nip { get; set; }
     }
 }
