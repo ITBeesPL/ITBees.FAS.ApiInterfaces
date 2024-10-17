39abc67a9f9d9a307d75dc90c8fb24144deae16a
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-10-17 11:28:52 +0200

    Fixes for agents, device hub setup

--- ITBees.FAS.ApiInterfaces/Companies/CompanyPlatformSubscriptionVm.cs
+++ ITBees.FAS.ApiInterfaces/Companies/CompanyPlatformSubscriptionVm.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using ITBees.Models.Companies;
 using ITBees.RestClient.Interfaces.RestModelMarkup;
 
 namespace ITBees.FAS.ApiInterfaces.Companies;
 
 public class CompanyPlatformSubscriptionVm : Vm
 {
+    public CompanyPlatformSubscriptionVm()
+    {
+        
+    }
+
+    public CompanyPlatformSubscriptionVm(CompanyPlatformSubscription x)
+    {
+        SubscriptionActiveTo = x.SubscriptionActiveTo;
+        SubscriptionPlanGuid = x.SubscriptionPlanGuid;
+        TrialNotAvailable = x.TrialNotAvailable;
+        SubscriptionPlanName = x.SubscriptionPlanName;
+    }
+
     public Guid? SubscriptionPlanGuid { get; set; }
     public string? SubscriptionPlanName { get; set; }
     public DateTime? SubscriptionActiveTo { get; set; }
--- ITBees.FAS.ApiInterfaces/Companies/CompanyVm.cs
+++ ITBees.FAS.ApiInterfaces/Companies/CompanyVm.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+using ITBees.Models.Companies;
 using ITBees.RestClient.Interfaces.RestModelMarkup;
 
 namespace ITBees.FAS.ApiInterfaces.Companies
 {
     public class CompanyVm : Vm
     {
+        public CompanyVm()
+        {
+            
+        }
+
+        public CompanyVm(Company x)
+        {
+            Guid = x.Guid;
+            CompanyName = x.CompanyName;
+            CreatedBy = x.CreatedBy?.DisplayName;
+            Created = x.Created;
+            CreatedByGuid = x.CreatedByGuid;
+            Owner = x.Owner?.DisplayName;
+            OwnerGuid = x.OwnerGuid;
+            IsActive = x.IsActive;
+            CompanyShortName = x.CompanyShortName;
+            Street = x.Street;
+            PostCode = x.PostCode;
+            City = x.City;
+            Nip = x.Nip;
+            CompanyPlatformSubscription = new CompanyPlatformSubscriptionVm(x.CompanyPlatformSubscription);
+        }
         public Guid Guid { get; set; }
         public string CompanyName { get; set; }
         public DateTime Created { get; set; }
